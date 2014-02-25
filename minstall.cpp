74ad2da3094ef53edc2fecb3ecc4ed952969bb86
anticapitalista <antiX@operamail.com>
2014-02-26 00:48:42 +0200

    Update minstall.cpp
    us/uk/gb keyboard doesn't need ,us

--- minstall.cpp
+++ minstall.cpp
@@ -1441,7 +1441,11 @@ void MInstall::setLocale() {
   if (kb == "uk") {
     kb = "gb";
   }
+  if (kb == "us") {
+  cmd = QString("sed -i 's/.*us/XKBLAYOUT=\"%1/g' /mnt/antiX/etc/default/keyboard").arg(kb);
+  } else {
   cmd = QString("sed -i 's/.*us/XKBLAYOUT=\"%1,us/g' /mnt/antiX/etc/default/keyboard").arg(kb);
+  }
   system(cmd.toAscii());
 
   //locale
