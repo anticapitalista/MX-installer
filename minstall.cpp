28c46c3e9b5c6cd9e8fd1c3b327e750a2fa03268
Adrian @Home <adilecu@gmail.com>
2014-05-30 19:33:00 -0400

    resize colums treewidget

--- minstall.cpp
+++ minstall.cpp
@@ -342,7 +342,8 @@ MInstall::MInstall(QWidget *parent) : QWidget(parent) {
     computerGroupEdit->setEnabled(false);
     computerGroupEdit->setText("");
   }
-
+  csView->resizeColumnToContents(1);
+  csView->resizeColumnToContents(2);
 }
 
 MInstall::~MInstall() {
