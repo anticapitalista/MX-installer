bff4174bf94f94991dd25456de9fb37cb561f8c3
Adrian <adrian@mxlinux.org>
2015-10-09 17:12:37 -0400

    removed script added dependency on package

--- minstall.cpp
+++ minstall.cpp
@@ -2182,7 +2182,7 @@ void MInstall::on_diskCombo_activated() {
   removedItem = "";
 
   // build rootCombo
-  QString cmd = QString("gptdospartinfo %1 -e").arg(drv);
+  QString cmd = QString("partinfo %1 -e").arg(drv);
   FILE *fp = popen(cmd.toUtf8(), "r");
   int rcount = 0;
   if (fp != NULL) {
@@ -2221,7 +2221,7 @@ void MInstall::on_diskCombo_activated() {
   // build home and swap combo for all disks
   for (int i = 0; i < diskCombo->count(); ++i) {
     QString drv = QString("/dev/%1").arg(diskCombo->itemText(i));
-    QString cmd = QString("gptdospartinfo %1 -e").arg(drv);
+    QString cmd = QString("partinfo %1 -e").arg(drv);
     FILE *fp = popen(cmd.toUtf8(), "r");
     if (fp != NULL) {
       char *ndev, *nsz, *nsys, *label;
